5c5851dfad5a45a4708f7e643d646ad6653369eb
Abramovich88 <petrova2008@ymail.com>
2025-12-08 20:32:06 +0300

    docker2

--- app.py
+++ app.py
@@ -9,3 +9,9 @@ def mul(a: int, b: int):
 
 def popsa(a: int, b: int):
     return a / b
+if __name__ == "__main__"
+    print(greet("Stas"))
+    print("2 + 3 =", add(2, 3))
+    print("2 * 5 =", mul(2, 5))
+    print("6 / 3 =", popsa(6, 3))
+        
